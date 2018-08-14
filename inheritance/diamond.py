b3e085b3ac52d1e0f4498bdbf23b84caa921cd4a
Alexander Bochkarev <abochkarev@mirantis.com>
2018-08-14 14:53:27 +0400

    Inheritance examples.

---
+++ inheritance/diamond.py
@@ -0,0 +1,35 @@
+class A:
+    def ping(self):
+        print ('ping:', self)
+
+
+class B:
+    def ping(self):
+        print ('pong', self)
+
+
+class C(A):
+    def pong(self):
+        print ('PONG', self)
+
+
+class D(B, C):
+    
+    def ping(self):
+        super().ping()
+        print('post-ping:', self)
+
+    def pingpong(self):
+        self.ping()
+        super().ping()
+        self.pong()
+        super().pong()
+        C.pong(self)
+
+
+if __name__ == "__main__":
+    d = D()
+    d.pingpong()
+    d.pingpong()
+
+
---
+++ inheritance/dict.py
@@ -0,0 +1,32 @@
+import collections
+
+
+class AnwerDict(dict):
+    def __getitem__(self, key):
+        return 32
+
+class DoppelDict2(collections.UserDict):
+    def __setitem__(self, key, value):
+        super().__setitem__(key, [value] * 2)
+
+if __name__ == '__main__':
+    print ('Answer dict == wrong behavior')
+    ad = AnwerDict(a='foo')
+    print (ad['a'])
+
+    d = {}
+    d.update(ad)
+    print (d['a'])
+
+    print (d)
+    print ('Doppel dict == right behavior')
+
+    dd = DoppelDict2(one=1)
+    print (dd)
+
+    dd['two'] = 2
+    print (dd)
+
+    dd.update(three=3)
+    print (dd)
+    
