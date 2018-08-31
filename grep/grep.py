da0a37d20c25d252c113881497de6b59f5b2d23a
Alexander Bochkarev <abochkarev@mirantis.com>
2018-08-31 11:09:53 +0400

    Add grep example.

---
+++ grep/grep.py
@@ -0,0 +1,12 @@
+import sys
+
+def grep(file_name, string):
+    with open(file_name) as f:
+        for l in f:
+            if string in l:
+                yield l.replace('\n', '').strip()
+
+if __name__ == '__main__':
+    for i in grep(sys.argv[1], sys.argv[2]):
+        print(i)
+
